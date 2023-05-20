8770d720c5fc446e7c5e3bc53451e53e2f483e34
Moseh-25-sudo <mosesnganga186@gmail.com>
2023-05-20 05:14:35 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,7 +15,7 @@ int main(void)
 	for (d = '0'; d <= '9'; d++)
 	{
 	putchar(d);
-	putchar(d != '9');
+	if (d != '9');
 	{
 	putchar(';');
 	putchar(' ');
@@ -24,4 +24,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
