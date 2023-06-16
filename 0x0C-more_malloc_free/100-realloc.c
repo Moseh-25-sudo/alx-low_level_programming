0803cc3de27d0aa60a472a9228a5808b21715c6e
Moseh-25-sudo <mosesnganga186@gmail.com>
2023-06-16 03:21:47 -0700

    correction

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,34 +16,34 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (new_size == old_size)
-		return (ptr);
+	return (ptr);
 
 	if (ptr == NULL)	
 	{
-		nptr = malloc(new_size);
+	nptr = malloc(new_size);
 
-		if (nptr == NULL)
-			return (NULL);
+	if (nptr == NULL)
+	return (NULL);
 
-		return (nptr);
+	return (nptr);
 	}
 	else
 	{
-		if (new_size == 0)
-		{
-			free(ptr);
-			return (NULL);
-		}
+	if (new_size == 0)
+	{
+	free(ptr);
+	return (NULL);
+	}
 	}
 
 	nptr = malloc(new_size);
 
 	if (nptr == NULL)
-		return (NULL);
+	return (NULL);
 
 	for (i = 0; i < old_size && i < new_size; i++)
 	{
-		nptr[i] = ((char *) ptr)[i];
+	nptr[i] = ((char *) ptr)[i];
 	}
 
 	free(ptr);
