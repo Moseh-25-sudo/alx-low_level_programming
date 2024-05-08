06f9b66a92772882500edac66a635694ddbe6587
Moseh-25-sudo <mosesnganga186@gmail.com>
2024-05-08 13:40:07 -0700

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - program checks for lowercase charscters
+ * @c: the character to check
+ * Return: 1 if c is lowercase, and 0 if it's not
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
